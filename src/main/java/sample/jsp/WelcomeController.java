5b8ed6a5404b512a8a667364425e713ecea28471
Philip Tenn <ptenn@users.noreply.github.com>
2016-01-12 21:26:50 -0500

    Re-run Gradle Wrapper for Gradle 2.10.  Replace logback with log4j2.  Fix application.properties so it uses = instead of : (yaml).

--- src/main/java/sample/jsp/WelcomeController.java
+++ src/main/java/sample/jsp/WelcomeController.java
@@ -19,6 +19,8 @@ package sample.jsp;
 import java.util.Date;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,11 +28,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController {
 
+	private static final Logger log = LoggerFactory.getLogger(WelcomeController.class);
+
 	@Value("${application.message:Hello World}")
 	private String message = "Hello World";
 
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
+        log.info("Called welcome controller");
 		model.put("time", new Date());
 		model.put("message", this.message);
 		return "welcome";
